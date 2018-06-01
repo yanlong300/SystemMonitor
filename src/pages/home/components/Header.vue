1a0cbb288918a3a697f7b503fc85e4f5138baded
闫龙 <yan.long@ielpm.com>
2018-06-01 13:36:40 +0800

    init project

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header">
+      欢迎来到监控系统
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped>
+  .header{
+    height: 1.86rem;
+    line-height: 1.86rem;
+    background-color: #000;
+    color: white;
+  }
+</style>
